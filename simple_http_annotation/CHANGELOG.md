c2f33cd4edff985bab8c86c59d55a08a4de4c7cd
Seongjun Kim <seongjki@c10r3s14.42seoul.kr>
2023-10-08 16:48:41 +0900

    Fix: Seperate Annotation from simple_http_gen to simple_http_annotation

--- simple_http_annotation/CHANGELOG.md
+++ simple_http_annotation/CHANGELOG.md
@@ -1,4 +1,3 @@
 ## 0.0.1
 
 - Initial version. This Annotation is used with simple_http_gen
-
--- simple_http_annotation/test/simple_http_annotation_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:simple_http_annotation/simple_http_annotation.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
